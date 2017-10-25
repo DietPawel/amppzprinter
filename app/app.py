994f65988ae4043b551eff15c5d1ba6d71490120
Pawel D <pawel.dietrich@gmail.com>
2017-10-25 21:41:09 +0200

    testing 2

--- app/app.py
+++ app/app.py
@@ -1,40 +1,33 @@
-from flask import Flask, make_response, abort ,request # Response
-import datetime,time
-import os
-#from subprocess import Popen, PIPE, STDOUT
+from flask import Flask, make_response, abort ,request
+import datetime,time, os, re
 from unidecode import unidecode
 
 DEBUG = False
+HOST = "localhost"
+PORT = 8000
+SOLVE = '127.0.0.1'
 
 app = Flask(__name__)
 
-def send_to_printer(header, code, syntax="text"):
-	header = "{} -- sala {}, komputer {}".format(header,"","")
+def send_to_printer(header, code, syntax="text", filename="AMPPZ 2017"):
+	#header = "{} -- sala {}, komputer {}".format(header,"","")
 	footer = "{}".format(datetime.datetime.fromtimestamp(time.time()).strftime("%d.%m.%Y %X"))
 	header, footer = unidecode(header), unidecode(footer)
-
+	with open('code','wb') as codefile:
+		codefile.write(code.encode("iso-8859-2", "replace"))
 	LANG_A2PS = {
 		'c': 'c',
 		'cpp': 'c++',
 		'pas': 'pascal',
 	}
-	with open('code','wb') as codefile:
-		codefile.write(code.encode("iso-8859-2", "replace"))
 	a2ps_lang = LANG_A2PS.get(syntax, None)
 	pretty_print = ""
 	if a2ps_lang is not None:
 		pretty_print = "-E{}".format(a2ps_lang)
-	res = os.system("a2ps %s -XISO-8859-2 --stdin=\"%s\" --header=\"%s\" --left-footer=\"%s\" code | lp "%
-		( pretty_print, "hardcoded filename", header, footer)
+	res = os.system("cat code | a2ps %s -XISO-8859-2 --stdin=\"%s\" --header=\"%s\" --left-footer=\"%s\" | lp "%
+		( pretty_print, filename, header, footer)
 	)
-	"""cmd = ["/usr/bin/a2ps","", pretty_print, "-XISO-8859-2",
-		"--stdin={}".format("test"),
-		"--header={}".format(header),
-		"--left-footer={}".format(footer)]
-	print(cmd)
-	p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
-	a,b = p.communicate(input=code.encode("iso-8859-2", "replace"))
-	print(b)"""
+	return res
 
 @app.route('/', methods=['GET'])
 def root():
@@ -46,14 +39,14 @@ def show_form():
     return make_response("""
 <form method="post">
 	<p><h3>tresc</h3>
-		<input type="contents" name="code"></input>
+		<input type="text" name="content"></input>
 		<!--<textarea rows="16" cols="80" name="text"></textarea>-->
 	</p>
 	<p><h3>syntax</h3>
 	<select name="lang">
-		<option value="cpp">cpp</option>
+		<option value="cpp">c++</option>
 		<option value="c">c</option>
-		<option value="pas">pas</option>
+		<option value="pas">pascal</option>
 		<option value="text">text</option>
 
 	</select>
@@ -65,21 +58,25 @@ def show_form():
 ### print form contents
 @app.route('/print/', methods=['POST'])
 def print_form():
-	#print(request.form)
-	code = request.form.get('contents')
+	if(not request.remote_addr == SOLVE):
+		abort(400)
+	print(request.form)
+	code = request.form.get('content')
 	syntax = request.form.get('lang')
+	filename = request.form.get('filename')
+	team_ip = request.form.get('ip')
+	team_name = request.form.get('name')
 	if code is None or syntax is None:
 		abort(400)
-	print(code+syntax)
-	send_to_printer(request.remote_addr,code,syntax)
-	return make_response('query ok')
+	#print(code+syntax)
+	res = send_to_printer(request.remote_addr,code,syntax)
+	return make_response('query result: '+str(res))
 
 
 ### other
 @app.errorhandler(404)
 def not_found(error):
-    #return make_response(jsonify({'error': 'Not found'}), 404)
     return make_response('<h1> 404 </h1>')
 
 if __name__ == '__main__':
-    app.run(debug=DEBUG,host="localhost", port=8000)
+    app.run(debug=DEBUG,host=HOST, port=PORT)
