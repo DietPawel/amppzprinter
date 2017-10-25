2015d3967f78515eb27fb9450d5c30a3f01bdef4
Pawel D <pawel.dietrich@gmail.com>
2017-10-25 19:52:56 +0200

    testing

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, make_response, abort,request, send_from_directory, redirect, render_template, Response
+from flask import Flask, make_response, abort ,request # Response
 import datetime,time
-from subprocess import Popen, PIPE, STDOUT
+import os
+#from subprocess import Popen, PIPE, STDOUT
 from unidecode import unidecode
 
 DEBUG = False
@@ -10,9 +11,8 @@ app = Flask(__name__)
 def send_to_printer(header, code, syntax="text"):
 	header = "{} -- sala {}, komputer {}".format(header,"","")
 	footer = "{}".format(datetime.datetime.fromtimestamp(time.time()).strftime("%d.%m.%Y %X"))
+	header, footer = unidecode(header), unidecode(footer)
 
-	header = unidecode(header)
-	footer = unidecode(footer)
 	LANG_A2PS = {
 		'c': 'c',
 		'cpp': 'c++',
@@ -22,14 +22,17 @@ def send_to_printer(header, code, syntax="text"):
 	pretty_print = ""
 	if a2ps_lang is not None:
 		pretty_print = "-E{}".format(a2ps_lang)
-	cmd = ["/usr/bin/a2ps", pretty_print, "-XISO-8859-2",
+	res = os.system("echo \"%s\" | a2ps %s -XISO-8859-2 --stdin=\"%s\" --header=\"%s\" --left-footer=\"%s\" | lp "%
+		(code.encode("iso-8859-2", "replace"),pretty_print, "hardcoded filename", header, footer)
+	)
+	"""cmd = ["/usr/bin/a2ps","", pretty_print, "-XISO-8859-2",
 		"--stdin={}".format("test"),
 		"--header={}".format(header),
 		"--left-footer={}".format(footer)]
 	print(cmd)
 	p = Popen(cmd, stdout=PIPE, stdin=PIPE, stderr=PIPE)
 	a,b = p.communicate(input=code.encode("iso-8859-2", "replace"))
-	print(b)
+	print(b)"""
 
 @app.route('/', methods=['GET'])
 def root():
@@ -40,7 +43,10 @@ def root():
 def show_form():
     return make_response("""
 <form method="post">
-	<p><h3>tresc</h3><input type="text" name="code"></input></p>
+	<p><h3>tresc</h3>
+		<input type="text" name="code"></input>
+		<!--<textarea rows="16" cols="80" name="text"></textarea>-->
+	</p>
 	<p><h3>syntax</h3>
 	<select name="syntax">
 		<option value="cpp">cpp</option>
@@ -50,19 +56,20 @@ def show_form():
 
 	</select>
 	</p>
-	<input type="submit"></input>	
+	<input type="submit"></input>
 </form>
 """)
 
 ### print form contents
 @app.route('/print/', methods=['POST'])
 def print_form():
+	print(request.form)
 	code = request.form.get('code')
 	syntax = request.form.get('syntax')
 	if code is None or syntax is None:
 		abort(400)
 	print(code+syntax)
-	send_to_printer(request.remote_addr,code,syntax)
+	#send_to_printer(request.remote_addr,code,syntax)
 	return make_response('query ok')
 
 
@@ -70,7 +77,7 @@ def print_form():
 @app.errorhandler(404)
 def not_found(error):
     #return make_response(jsonify({'error': 'Not found'}), 404)
-    return make_response('<center style="font-size:60vh;margin-top:20vh;"> 404 </center>')
+    return make_response('<h1> 404 </h1>')
 
 if __name__ == '__main__':
     app.run(debug=DEBUG,host="localhost", port=8000)
